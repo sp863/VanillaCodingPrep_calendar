878231a02ec82584245263629221ed848d315c2b
sp863 <sp863429@gmail.com>
2021-12-24 03:09:31 +0900

    calendar: week days html

--- script.js
+++ script.js
@@ -3,28 +3,13 @@
 const dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const daysCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let currentDate;
+const calendar = document.getElementById("calendar");
 
-currentDate = new Date();
-const currYear = currentDate.getFullYear();
-const currMonth = currentDate.getMonth();
-const currDate = currentDate.getDate();
-
-const firstDate = new Date(currYear, currMonth, 1);
-let year = firstDate.getFullYear(); // 2021
-let month = firstDate.getMonth(); // 12
-let date = firstDate.getDate(); // 1
-let day = firstDate.getDay(); // 0
-
-console.log("-----------------");
-console.log(year);
-console.log(month + 1);
-console.log(date);
-console.log(dayName[day]);
-
-let x = day;
-for (let i = 0; i < daysCount[month]; i++) {
-  console.log(`${i + 1} ${dayName[x]}`);
-  x++;
-  if (x > 6) x = 0;
-}
+// for (let i = 0; i < 6; i++) {
+//   const row = calendar.insertRow();
+//   for (let j = 0; j < 7; j++) {
+//     console.log(`row : ${row}`);
+//     const cell = row.insertCell();
+//     cell.innerHTML = "HI";
+//   }
+// }
